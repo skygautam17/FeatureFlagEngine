c662b0c71aef3c7583f4259959bb7b81c4fd7139
monu.pandey <info@optionpay.in>
2026-02-09 11:59:41 +0530

    Fixed Validation issue

--- src/FeatureFlagEngine.Web/ClientApp/src/app/features/features/features.component.ts
+++ src/FeatureFlagEngine.Web/ClientApp/src/app/features/features/features.component.ts
@@ -1,48 +1,80 @@
 import { Component, OnInit } from '@angular/core';
 import { FeatureApiService } from '../../features/services/feature-api.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-features',
   templateUrl: './features.component.html'
 })
 export class FeaturesComponent implements OnInit {
-  features:any[]=[];
-  newFeature:any={ key:'', enabled:false };
 
-    constructor(private api: FeatureApiService){}
+  features: any[] = [];
 
-  ngOnInit(){
-    debugger;
-     this.load(); 
-    }
+  newFeature: any = {
+    key: '',
+    description: '',
+    enabled: false
+  };
 
-  load(){
-    this.api.getFeatures().subscribe((res:any)=>this.features=res);
+  constructor(private api: FeatureApiService) {}
+
+  ngOnInit() {
+    this.load();
   }
 
-  create(){
-    this.api.createFeature(this.newFeature).subscribe(()=>{
-      this.newFeature={ key:'', enabled:false };
+  load() {
+    this.api.getFeatures().subscribe((res: any) => this.features = res);
+  }
+
+  create(form: NgForm) {
+
+    if (form.invalid) return;
+
+    this.api.createFeature(this.newFeature).subscribe(() => {
+
+      // Reset model
+      this.newFeature = {
+        key: '',
+        description: '',
+        enabled: false
+      };
+
+      // Clear validators + form state
+      form.resetForm(this.newFeature);
+
       this.load();
     });
   }
 
-  delete(id:string){
-    this.api.deleteFeature(id).subscribe(()=>this.load());
+  delete(id: string) {
+    this.api.deleteFeature(id).subscribe(() => this.load());
   }
 
   edit(feature: any) {
+  feature.original = { ...feature };
   feature.isEditing = true;
 }
 
-cancelEdit(feature: any) {
+cancelEdit(feature: any, form: NgForm) {
+  Object.assign(feature, feature.original);
   feature.isEditing = false;
-  this.load();   // reload original values
+  form.resetForm(feature);
+  this.load();
 }
 
-update(feature: any) {
+  update(feature: any, form: NgForm) {
+
+  if (form.invalid) {
+    form.control.markAllAsTouched();
+    return;
+  }
+
   this.api.updateFeature(feature).subscribe(() => {
     feature.isEditing = false;
+
+    // reset validation state
+    form.resetForm(feature);
+
     this.load();
   });
 }
